86d1f1c943651c6a26cec3fdc2380cbfb4a6c54e
ajitrai1507 <ajitrai1507@gmail.com>
2026-05-29 17:43:13 +0530

    Primitive data types

---
+++ 03.js
@@ -0,0 +1,28 @@
+let name = "Ajit Rai";
+console.log(name);
+console.log(typeof name)
+
+let num = 10;
+console.log(num)
+console.log(typeof num);
+
+let login = true;
+console.log(login)
+console.log(typeof login);
+
+let address = null;
+console.log(address)
+console.log(typeof address);
+
+let weatherReport = undefined;
+console.log(weatherReport)
+console.log(typeof weatherReport)
+
+
+let bigNum = 2323232323232232323232322323n;
+console.log(bigNum);
+console.log(typeof bigNum);
+
+let mySymbol = Symbol('description');
+console.log(mySymbol);
+console.log(typeof mySymbol);
