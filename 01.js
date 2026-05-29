37f5681e45f92e7ee458a3630c65d758507f5475
ajitrai1507 <ajitrai1507@gmail.com>
2026-05-29 17:05:45 +0530

    Hello World

--- 01.js
+++ 01.js
@@ -1,7 +1,1 @@
 console.log("Hello World");
-
-console.log("Ajit Rai");
-
-console.log("24");
-
-console.log("New Delhi");
--- 03.js
+++
@@ -1,8 +0,0 @@
-let name = "Ajit"
-let age = 24;
-let login = true;
-
-
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof login);
