feed994436bd8f7194286de3cbbdbd52446b5a80
ajitrai1507 <ajitrai1507@gmail.com>
2026-06-01 22:34:08 +0530

    Add examples for Nullish Coalescing and Optional Chaining

---
+++ 07.js
@@ -0,0 +1,47 @@
+// Nullish Coalescing (??)
+
+let name = null;
+// console.log(name ?? 'Guest');
+
+let age = 0;
+// console.log(age ?? 10)
+
+// console.log(0 || 10);
+// console.log(0 ?? 20)
+
+// Optional Chaining (?.)
+
+// const user = {}
+
+// console.log(user.address?.city);
+
+//Accessing Nested Objects
+
+// const user = {
+//     name : "Ajit",
+//     address: {
+//         city: 'Banglore'
+//     }
+// }
+
+// console.log(user.name);
+// console.log(user.address.city);
+// console.log(user.address.phone);
+// console.log(user.credential?.email);
+
+// const user = {};
+
+// console.log(user.address?.city ?? 'New Delhi');
+
+// console.log(null ?? 'Hello')
+
+// console.log(undefined ?? 'Ajit');
+
+// console.log(false ?? true);
+
+// console.log("" ?? "Default");
+
+// const user = {};
+
+// console.log(user.profile?.name ?? 'Ajit');
+
