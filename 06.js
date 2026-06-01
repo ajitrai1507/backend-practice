b6280f162b735a8cf06e1ea2e7ca4b7a575dddae
ajitrai1507 <ajitrai1507@gmail.com>
2026-06-01 22:14:02 +0530

    Add logical and assignment operator examples

---
+++ 06.js
@@ -0,0 +1,57 @@
+//Logical Operator
+
+// let a = 5, b=10;
+
+// console.log((a<b) && (b>0))
+
+// let x = 4, y = 8;
+
+// console.log((x>y) || (y>5))
+
+// console.log(!(10>5))
+
+// console.log((7 == 7) && (3 != 3))
+
+
+// console.log((4 < 2) || (9 > 3))
+
+// let a = 3, b = 6, c = 9;
+
+// console.log((a<b) && (b<c))
+
+// let age = 18;
+// console.log((age>=18) && (age<=60))
+
+
+// let marks = 75;
+// console.log((marks>=90) || marks<40);
+
+// let A = 10, B = 10
+
+// console.log(!(A && B))
+
+//Check Lear year
+
+//Assignment Operator
+
+// let x = 10;
+// x+=5;
+
+// console.log(x)
+
+// let x = 20;
+// x-= 8;
+// console.log(x)
+
+// let x = 5;
+// x*= 4;
+// console.log(x)
+
+// let x = 30;
+// x/= 6;
+// console.log(x)
+
+// let x = 17;
+// x%= 5;
+// console.log(x)
+
