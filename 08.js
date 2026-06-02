3f36805aa3f1bbf1068ec625f68f108c83cb4c1e
ajitrai1507 <ajitrai1507@gmail.com>
2026-06-02 22:14:04 +0530

    if / else if / else control flow

---
+++ 08.js
@@ -0,0 +1,67 @@
+// let age= 20;
+// if(age>=20){
+//     console.log("You can vote");
+// }
+
+// let age = 16;
+
+// if(age>=18){
+//     console.log("You can vote");
+// }else{
+//     console.log("You can't vote");
+// }
+
+// let marks = 40;
+
+// if(marks>=90){
+//     console.log("You're Grade is A");
+// }else if(marks>=75){
+//     console.log("You're Grade is B");
+// }else if(marks>=50){
+//     console.log("You're Grade is C");
+// }else{
+//     console.log("You're Grade is D")
+// }
+
+// let num = 0;
+
+// if(num>0){
+//     console.log("Number is positive");
+// }else if(num<0){
+//     console.log("Number is negative");
+// }else if(num===0){
+//     console.log("Number is zero");
+// }else{
+//     console.log("Not a Number");
+// }
+
+// let num = 11;
+
+// if(num%2==0){
+//     console.log("Number is Even");
+// }else{
+//     console.log("Number is Odd");
+// }
+
+// let loggedIn = false;
+
+// if(loggedIn==true){
+//     console.log("User is login");
+// }else{
+//     console.log("User is not login");
+// }
+
+// let age = 18;
+
+// if(age>=18){
+//     console.log("You're Adult");
+// }
+
+let age = 19;
+let hasLicensed = true;
+
+if(age>=18 && hasLicensed){
+    console.log("You can Drive a car");
+}else{
+    console.log("You cant drive a car")
+}
