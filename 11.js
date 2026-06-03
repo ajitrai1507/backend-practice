7d3b84af6f4f93893a7e81b1df37cd9fe9e7ab10
ajitrai1507 <ajitrai1507@gmail.com>
2026-06-03 08:49:21 +0530

    loops, break and continue

---
+++ 11.js
@@ -0,0 +1,37 @@
+//For Loop
+// for (initialization; condition; increment/decrement) {
+//   // code
+// }
+
+// for (let i=1; i<=5; i++){
+//     console.log(i);
+// }
+
+// for(let i =1 ; i<=20; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+
+// for(let i=1; i<=20; i++){
+//     if(i==5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+// for (let i=1; i<=10; i++){
+//     if(i==6){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+let i = 1;
+
+while(i<=5){
+    console.log(i);
+    i++;
+}
