4f156e3010dbce96eb806b078269341f4bca8699
ajitrai1507 <ajitrai1507@gmail.com>
2026-06-03 08:27:42 +0530

    switch statement

---
+++ 09.js
@@ -0,0 +1,60 @@
+// let day = 1;
+
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+    
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+    
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+    
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+// let grade = 'D';
+
+// switch(grade){
+//     case 'A':
+//         console.log("Excellent");
+//         break;
+//     case 'B':
+//         console.log("Good");
+//         break;
+//     case 'C':
+//         console.log("Average");
+//         break;
+//     default:
+//         console.log("Failure");
+// }
+
+// let day = 'Sunday';
+
+// switch(day){
+//     case 'Saturday':
+//     case 'Sunday':
+//         console.log("Weekend");
+//         break;
+//     case 'Monday':
+//         console.log("Office jaao");
+//         break;
+//     default:
+//         console.log("Office jaa bc....")
+// }
