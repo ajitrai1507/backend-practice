c69c0be6625314fcd5a24e8ff59eb9e8204565c7
ajitrai1507 <ajitrai1507@gmail.com>
2026-06-03 08:43:17 +0530

    Ternary operator

---
+++ 10.js
@@ -0,0 +1,22 @@
+//Ternary operator
+
+// condition ? expressionIfTrue : expressionIfFalse
+
+
+// let age = 20;
+
+// console.log(age>=18 ? "Adult" : "Teenager")
+
+// let age = 18;
+// let hasLincenced = false;
+
+
+// console.log(age>=18 && hasLincenced ? "You can drive" : "You can't drive");
+
+// let marks = 25;
+
+// console.log(marks>=35 ? "You're passed" : "You're Failed");
+
+let num = 9;
+
+console.log(num%2==0 ? "Even number" : "Odd number");
